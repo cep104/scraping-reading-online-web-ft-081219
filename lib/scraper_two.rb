7ad4d5eb0fa58ffe23a51dc0830c573f08adab64
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-09 20:39:11 +0000

    Automatically backed up by Learn

--- lib/scraper_two.rb
+++ lib/scraper_two.rb
@@ -4,4 +4,7 @@ require 'open-uri'
 html = open("http://flatironschool.com/")
 doc = Nokogiri::HTML(html)
  
-puts doc.css(".title-3Kf9MY")
+courses = doc.css(".title-3Kf9MY")
+courses.each do |course|
+  puts course.text.strip 
+end
